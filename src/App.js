41862ef603f7a9048546c14b73e4528e2602a911
sbuteryd@qq.com <sbuteryd@qq.com>
2019-05-29 23:39:28 +0800

    3-1 设置一个hello World

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  React,{Component} from 'react'
+
+
+export  default class App extends React.Component {
+    render() {
+        return(
+            <div className='app'>
+                Hello World
+            </div>
+        )
+    }
+}
