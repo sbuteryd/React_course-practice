1dec1bb459a77725a9f7803d2d7ac7e41e3913f9
sbuteryd@qq.com <sbuteryd@qq.com>
2019-05-29 22:35:18 +0800

    1-2 第一个组件

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import React , {Component} from 'react';
 
 class ContactList extends  React.Component{
   render() {
-    const people = [
-      {name:'michael'},
-      {name:'Ryan'},
-      {name:'Tyler'}
-    ]
+    const people = this.props.contacts
+
     return (<ol>
       {people.map(person =>(
           <li>{person.name}</li>
@@ -17,11 +14,21 @@ class ContactList extends  React.Component{
   }
 }
 
+
 class App extends Component{
   render() {
     return (
         <div className='App'>
-          <ContactList/>
+          <ContactList contacts={[
+            {name:'michael'},
+            {name:'Ryan'},
+            {name:'Tyler'}
+          ]}/>
+          <ContactList contacts={[
+            {name:'russell'},
+            {name:"lihangen"},
+            {name:"somePeople"}
+          ]}/>
         </div>
     )
   }
