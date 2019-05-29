a6a12b96d6325c4cd4d83bbacf3889b900b8438a
sbuteryd@qq.com <sbuteryd@qq.com>
2019-05-29 22:26:15 +0800

    1-1 第一个组件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class ContactList extends  React.Component{
+  render() {
+    const people = [
+      {name:'michael'},
+      {name:'Ryan'},
+      {name:'Tyler'}
+    ]
+    return (<ol>
+      {people.map(person =>(
+          <li>{person.name}</li>
+          )
+      )}
+    </ol>)
+  }
 }
+
+class App extends Component{
+  render() {
+    return (
+        <div className='App'>
+          <ContactList/>
+        </div>
+    )
+  }
+}
+
 
 export default App;
