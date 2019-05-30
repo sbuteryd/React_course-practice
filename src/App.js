01cdbcf2e0b4db4a2f8829801cde82fd440fd8f0
sbuteryd@qq.com <sbuteryd@qq.com>
2019-05-30 08:00:50 +0800

    3-3 练习第一次 使用props传递数据

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
     render() {
         return(
             <div>
+                <ListConteacts contacts={contacts} />
             </div>
         )
     }
--- src/ListConteacts.js
+++ src/ListConteacts.js
@@ -2,7 +2,24 @@ import  React,{ Component } from  'react'
 
 class ListConteacts extends Component {
     render() {
+        const list = this.props.contacts
+        return(
+            <ol className='contact-list'>
+                {list.map((pare)=>(
+                    <li  key={pare.id} className='contact-list-item'>
+                        <div className='contact-avatar'
+                             style={{backgroundImage:`url(${pare.avatarURL})`}}
+                        />
+                        <div className='contact-details'>
+                            <p>{pare.name}</p>
+                            <p>{pare.email}</p>
+                        </div>
+                        <button className='contact-remove'>remove</button>
+                    </li>
 
+                ))}
+            </ol>
+        )
     }
 }
 
