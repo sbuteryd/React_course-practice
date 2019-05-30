905b3a44d201bbe03eec4d324c516611103ef5fd
sbuteryd@qq.com <sbuteryd@qq.com>
2019-05-30 08:21:18 +0800

    4-2 组件内添加状态

--- src/App.js
+++ src/App.js
@@ -2,34 +2,38 @@ import  React,{Component} from 'react'
 import ListConteacts from './ListConteacts'
 
 class App extends Component {
+    state = {
+        contacts : [
+            {
+                "id": "ryan",
+                "name": "Ryan Florence",
+                "email": "ryan@reacttraining.com",
+                "avatarURL": "http://localhost:5001/ryan.jpg"
+            },
+            {
+                "id": "michael",
+                "name": "Michael Jackson",
+                "email": "michael@reacttraining.com",
+                "avatarURL": "http://localhost:5001/michael.jpg"
+            },
+            {
+                "id": "tyler",
+                "name": "Tyler McGinnis",
+                "email": "tyler@reacttraining.com",
+                "avatarURL": "http://localhost:5001/tyler.jpg"
+            }
+        ]
+    }
     render() {
+
         return(
             <div>
-                <ListConteacts list ={contacts}/>
+                <ListConteacts list ={this.state.contacts}/>
             </div>
         )
     }
 }
 
 
-const contacts = [
-    {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-    },
-    {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-    },
-    {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-]
+
 export  default App
