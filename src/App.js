3574354f020912c2a5ed69f974f6f841d8047cca
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-02 00:22:28 +0800

    4-1 删除数据

--- src/App.js
+++ src/App.js
@@ -3,27 +3,8 @@ import ListConteacts from './ListConteacts'
 
 class App extends Component {
     state = {
-        contacts : [
-            {
-                "id": "ryan",
-                "name": "Ryan Florence",
-                "email": "ryan@reacttraining.com",
-                "avatarURL": "http://localhost:5001/ryan.jpg"
-            },
-            {
-                "id": "michael",
-                "name": "Michael Jackson",
-                "email": "michael@reacttraining.com",
-                "avatarURL": "http://localhost:5001/michael.jpg"
-            },
-            {
-                "id": "tyler",
-                "name": "Tyler McGinnis",
-                "email": "tyler@reacttraining.com",
-                "avatarURL": "http://localhost:5001/tyler.jpg"
-            }
-        ]
-    }
+        contacts : []
+    };
     removeContact = (contact) =>{
         console.log(contact)
         this.setState((state)=>({
